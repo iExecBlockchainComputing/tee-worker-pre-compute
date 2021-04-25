1f081f828fb3fbd73f2408af8e4b3707445c1d56
Zied <26070035+zguesmi@users.noreply.github.com>
2021-04-25 20:37:35 +0200

    Download input files

--- src/main/java/com/iexec/worker/tee/pre/PreComputeArgs.java
+++ src/main/java/com/iexec/worker/tee/pre/PreComputeArgs.java
@@ -16,21 +16,63 @@
 
 package com.iexec.worker.tee.pre;
 
+import com.iexec.common.precompute.PreComputeExitCode;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.iexec.common.precompute.PreComputeUtils.*;
+import static com.iexec.common.utils.IexecEnvUtils.*;
+
+@Slf4j
 @Data
 @Builder
-@NoArgsConstructor
 @AllArgsConstructor
 public class PreComputeArgs {
 
+    
     private String chainTaskId;
+    private String outputDir;
+    // dataset
+    private boolean isDatasetRequired;
     private String encryptedDatasetUrl;
     private String encryptedDatasetBase64Key;
     private String encryptedDatasetChecksum;
     private String plainDatasetFilename;
-    private String outputDir;
+    // input files
+    private List<String> inputFiles;
+
+    public static PreComputeArgs readArgs() throws PreComputeException {
+        PreComputeArgs args = PreComputeArgs.builder()
+                .chainTaskId(getEnvVarOrThrow(IEXEC_TASK_ID))
+                .outputDir(getEnvVarOrThrow(IEXEC_PRE_COMPUTE_OUT))
+                .isDatasetRequired(Boolean.valueOf(getEnvVarOrThrow(IS_DATASET_REQUIRED)))
+                .inputFiles(new ArrayList<>())
+                .build();
+        if (args.isDatasetRequired()) {
+            args.setEncryptedDatasetUrl(getEnvVarOrThrow(IEXEC_DATASET_URL));
+            args.setEncryptedDatasetBase64Key(getEnvVarOrThrow(IEXEC_DATASET_KEY));
+            args.setEncryptedDatasetChecksum(getEnvVarOrThrow(IEXEC_DATASET_CHECKSUM));
+            args.setPlainDatasetFilename(getEnvVarOrThrow(IEXEC_DATASET_FILENAME));
+        }
+        int inputFilesNb = Integer.valueOf(getEnvVarOrThrow(IEXEC_INPUT_FILES_NUMBER));
+        for (int i = 1; i <= inputFilesNb; i++) {
+            String url = getEnvVarOrThrow(IEXEC_INPUT_FILE_URL_PREFIX + i);
+            args.getInputFiles().add(url);
+        }
+        return args;
+    }
+
+    public static String getEnvVarOrThrow(String envVarName) throws PreComputeException {
+        String envVar = System.getenv(envVarName);
+        if (envVar == null || envVar.isEmpty()) {
+            log.error("Required env var is empty [name:{}]", envVarName);
+            throw new PreComputeException(PreComputeExitCode.EMPTY_REQUIRED_ENV_VAR);
+        }
+        return envVar;
+    }
 }
