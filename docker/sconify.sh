fcf43708b8516e022019bea443322e91a34a53be
Jérémy James Toussaint <jeremy.toussaint@iex.ec>
2021-05-31 15:25:09 +0200

    Update sconify script

--- docker/sconify.sh
+++ docker/sconify.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-# ./sconify.sh docker.io/iexechub/tee-worker-pre-compute:dev docker.io/iexechub/tee-worker-pre-compute:dev-debug
+# IMG_FROM=docker.io/iexechub/tee-worker-pre-compute:dev IMG_TO=docker.io/iexechub/tee-worker-pre-compute:dev-debug ./sconify.sh
 cd $(dirname $0)
-
-IMG_FROM=$1
-IMG_TO=$2
 
 ARGS=$(sed -e "s'\${IMG_FROM}'${IMG_FROM}'" -e "s'\${IMG_TO}'${IMG_TO}'" sconify.args)
 echo $ARGS
