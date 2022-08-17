53b3e0f6e44bb3af26c5c3246949f971fa45f561
Jeremy Bernard <jeremy.bernard@iex.ec>
2022-08-17 15:58:59 +0200

    Update sconification tooling on local environment

--- docker/sconify.sh
+++ docker/sconify.sh
@@ -2,9 +2,23 @@
 # IMG_FROM=docker.io/iexechub/tee-worker-pre-compute:dev IMG_TO=docker.io/iexechub/tee-worker-pre-compute:dev-debug ./sconify.sh
 cd $(dirname $0)
 
+SCONE_IMG_NAME=scone-debug/iexec-sconify-image-unlocked
+SCONE_IMG_VERSION=5.7.1
+
+IMG_TO=${IMG_TO}-sconify-${SCONE_IMG_VERSION}-debug
+
 ARGS=$(sed -e "s'\${IMG_FROM}'${IMG_FROM}'" -e "s'\${IMG_TO}'${IMG_TO}'" sconify.args)
 echo $ARGS
 
-/bin/bash -c "docker run --rm -v /var/run/docker.sock:/var/run/docker.sock \
-            registry.scontain.com:5050/scone-debug/iexec-sconify-image-unlocked:5.3.12 \
-            sconify_iexec $ARGS"
+SCONE_IMAGE="registry.scontain.com:5050/${SCONE_IMG_NAME}:${SCONE_IMG_VERSION}"
+
+/bin/bash -c "docker run -t --rm \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    ${SCONE_IMAGE} \
+        sconify_iexec \
+            --cli=${SCONE_IMAGE} \
+            --crosscompiler=${SCONE_IMAGE} \
+            $ARGS"
+
+echo
+docker run --rm -e SCONE_HASH=1 $IMG_TO
